e8b9e5d8aa857620bc087052c191a2f478c6c8aa
Pronoy Roy <pronoyroy96@gmail.com>
2019-12-28 18:47:44 +0530

    project2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    FlexLayoutModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    id:string;
+    name:string;
+    image:string;
+    price:string;
+    description:string;
+    featured:boolean;
+    label:string;
+    category:string;
+}
